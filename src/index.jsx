c6d42c7f2d42bf645b9c84f35e93fdcc92044f5d
Esther Adegbaju <princessadeiye@gmail.com>
2023-12-16 01:59:13 +0100

    setup UI with material design styling

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,11 @@ import './styles/index.css';
 import App from './app/app.jsx';
 import Store from './app/store.jsx';
 import { Provider } from 'react-redux';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 
 const root = document.getElementById('root');

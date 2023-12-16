fd741aee3d73a5e99c80b16ee5c53c9008d8defb
Esther Adegbaju <princessadeiye@gmail.com>
2023-12-16 01:24:14 +0100

    refactoring

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './components/common/Header';
-import Footer from './components/common/Footer';
-import Home from './pages/Home';
-import AboutUs from './components/AboutUs';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from '../components/common/Header';
+import Footer from '../components/common/Footer';
+import Home from '../Pages/Home';
+import AboutUs from '../components/common/AboutUs';
 
 const App = () => {
   return (
     <Router>
       <div className="app-container">
         <Header />
-        <Switch>
+        <Routes>
           <Route exact path="/" component={Home} />
           <Route path="/about" component={AboutUs} />
-        </Switch>
+        </Routes>
         <Footer />
       </div>
     </Router>
---
+++ src/app/store.jsx
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+// import todosReducer from '../features/todos/todosSlice'
+// import filtersReducer from '../features/filters/filtersSlice'
+
+const store = configureStore({
+    reducer: {
+        // todos: todosReducer,
+        // filters: filtersReducer
+    }
+})
+
+export default store

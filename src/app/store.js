2fda178cf9c8c69cb5be1f62c60a2671de5364c7
Esther Adegbaju <princessadeiye@gmail.com>
2023-12-17 16:48:35 +0100

    refactor

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import TaskReducer from '../reducers/TaskReducer'
+import TaskReducer from './TaskReducer'
 
 const store = configureStore({
     reducer: {

d86eec5cbde6ec284b66c4e48bac25da70a97f4b
Esther Adegbaju <princessadeiye@gmail.com>
2023-12-17 16:39:35 +0100

    refactor

--- src/app/app.js
+++ src/app/app.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
-import Home from '../pages/Home';
+import Home from '../components/common/Home';
 import AboutUs from '../components/common/AboutUs';
-import Store from '../pages/Store';
+import Store from '../components/common/Store';
 import { Container } from '@mui/material';
 
 const App = () => {
